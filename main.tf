7a1cb87e84a7841387fd508ca174f2b07957e34b
Daeric <daeric@hashicorp.com>
2022-12-09 03:10:38 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ provider "oci" {
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_ocid
   display_name   = "My internal VCN"
 }
