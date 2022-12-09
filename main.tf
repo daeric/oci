379cc52ee9d1591887527c0e8d3a19385c4f6fff
Daeric <daeric@hashicorp.com>
2022-12-09 02:56:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
   }
 }
 
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+}
+
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
