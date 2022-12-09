2a1657deee23cb0efcd9bf35248022683712bc7b
Daeric <daeric@hashicorp.com>
2022-12-09 03:08:48 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ provider "oci" {
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
 }
 
 resource "oci_core_vcn" "internal" {
